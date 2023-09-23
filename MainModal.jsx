0cca753c97b8a4f8ad352dbfb748a6cb005395f0
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2023-09-24 04:14:17 +0600

    -added

--- MainModal.jsx
+++ MainModal.jsx
@@ -9,6 +9,8 @@ import { Link } from "react-router-dom";
 function MainModal({ activeModal, setActiveModal }) {
   const [contacts, setContacts] = useState([]);
   const [showEven, setShowEven] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [detailsModal, setDetailsModal] = useState(null);
   useEffect(() => {
     axios("https://contact.mediusware.com/api/contacts/")
       .then((res) => {
@@ -18,13 +20,14 @@ function MainModal({ activeModal, setActiveModal }) {
         console.log(err);
       });
   }, []);
-  console.log(showEven);
+
   return (
     <Modal
       show
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      style={{ position: "relative !important" }}
     >
       <Modal.Header className="flex">
         <Modal.Title id="contained-modal-title-vcenter" className="">
@@ -76,7 +79,17 @@ function MainModal({ activeModal, setActiveModal }) {
           })
           .filter((item) => (showEven ? item.id % 2 == 0 && item : item))
           .map((contact, index) => (
-            <div key={index} onClick={contact}>
+            <div
+              key={index}
+              onClick={() => setDetailsModal(true)}
+              style={{
+                position: "relative",
+                cursor: "pointer",
+                border: "1px solid black",
+                padding: "5px",
+                margin: "6px 0",
+              }}
+            >
               <small>{contact.id}</small>
               <h3>{contact.country.name}</h3>
               <p>Phone: {contact.phone}</p>
@@ -92,6 +105,40 @@ function MainModal({ activeModal, setActiveModal }) {
           onChange={() => setShowEven(!showEven)}
         />
       </Modal.Footer>
+      {detailsModal && (
+        <Modal
+          show
+
+          //   style={{
+          //     position: "absolute",
+          //     top: "5%",
+          //     left: "50%",
+          //     transform: "translateX(-50%)",
+          //     width: "60%",
+          //     height: "250px",
+          //     zIndex: "9999",
+          //     backgroundColor: "red",
+          //   }}
+        >
+          <div className="p-2">
+            <h4>Details</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium soluta, tempora aut, atque perferendis iusto nemo
+              ducimus excepturi nisi optio animi. Incidunt corporis iusto
+              facilis animi debitis sint eveniet dolorum nisi totam, architecto,
+              nemo reiciendis voluptatibus quia molestias non molestiae error
+              obcaecati quas. A, necessitatibus. Consequuntur incidunt in beatae
+              voluptas?
+            </p>
+          </div>
+          <Modal.Footer>
+            <Button variant="danger" onClick={() => setDetailsModal(null)}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
     </Modal>
   );
 }
